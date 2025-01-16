5fe1d6634ca52bce8f13e9f4b88a3ac755258ec7
intinek <102937646+intinek@users.noreply.github.com>
2025-01-16 18:58:29 -0300

    Update Main.java

--- src/main/java/com/alura/comex/Main.java
+++ src/main/java/com/alura/comex/Main.java
@@ -1,13 +1,8 @@
 package com.alura.comex;
 
 import java.io.IOException;
-import java.math.BigDecimal;
-import java.math.RoundingMode;
 import java.net.URISyntaxException;
-import java.text.NumberFormat;
-import java.time.LocalDate;
 import java.util.List;
-import java.util.Locale;
 
 public class Main {
 
@@ -15,36 +10,12 @@ public class Main {
         ProcesadorDeCsv procesador = new ProcesadorDeCsv();
         List<Pedido> pedidos = procesador.leerPedidosDesdeCsv("pedidos.csv");
 
-        int totalDeProductosVendidos = 0;
-        int totalDePedidosRealizados = 0;
-        BigDecimal montoDeVentas = BigDecimal.ZERO;
-        Pedido pedidoMasBarato = null;
-        Pedido pedidoMasCaro = null;
+        InformeSintetico informe = new InformeSintetico(pedidos);
 
-        CategoriasProcesadas categoriasProcesadas = new CategoriasProcesadas();
-        int totalDeCategorias = 0;
-
-        for (Pedido pedidoActual : pedidos) {
-            if (pedidoMasBarato == null || pedidoActual.getPrecio().multiply(new BigDecimal(pedidoActual.getCantidad())).compareTo(pedidoMasBarato.getPrecio().multiply(new BigDecimal(pedidoMasBarato.getCantidad()))) < 0) {
-                pedidoMasBarato = pedidoActual;
-            }
-
-            if (pedidoMasCaro == null || pedidoActual.getPrecio().multiply(new BigDecimal(pedidoActual.getCantidad())).compareTo(pedidoMasCaro.getPrecio().multiply(new BigDecimal(pedidoMasCaro.getCantidad()))) > 0) {
-                pedidoMasCaro = pedidoActual;
-            }
-
-            montoDeVentas = montoDeVentas.add(pedidoActual.getPrecio().multiply(new BigDecimal(pedidoActual.getCantidad())));
-            totalDeProductosVendidos += pedidoActual.getCantidad();
-            totalDePedidosRealizados++;
-
-            if (!categoriasProcesadas.contains(pedidoActual.getCategoria())) {
-                totalDeCategorias++;
-                categoriasProcesadas.add(pedidoActual.getCategoria());
-            }
-        }
-
-        System.out.println("#### INFORME DE VALORES TOTALES");
-        System.out.printf("- TOTAL DE PEDIDOS REALIZADOS: %s\n", totalDePedidosRealizados);
-        System.out.printf("- TOTAL DE PRODUCTOS VENDIDOS: %s\n", totalDeProductosVendidos);
-        System.out.printf("- TOTAL DE CATEGORIAS: %s\n", totalDeCategorias);
-        System.out.printf("- MONTO DE VENTAS: %s\n", NumberFormat.getCurrency
+        System.out.println("#### INFORME SINTÉTICO");
+        System.out.printf("- TOTAL DE PEDIDOS REALIZADOS: %s\n", informe.getTotalDePedidosRealizados());
+        System.out.printf("- TOTAL DE PRODUCTOS VENDIDOS: %s\n", informe.getTotalDeProductosVendidos());
+        System.out.printf("- TOTAL DE CATEGORÍAS: %s\n", informe.getTotalDeCategorias());
+        System.out.printf("- MONTO DE VENTAS: %s\n", informe.getMontoDeVentas());
+        System.out.printf("- PEDIDO MÁS BARATO: %s\n", informe.getPedidoMasBarato());
+        System.out.printf("- PEDIDO MÁS CARO: %s\n", informe.getPedido
