7a6ff6ea0673462d425a7a0f3b0b3b65e7eb6fad
intinek <102937646+intinek@users.noreply.github.com>
2025-01-16 19:41:21 -0300

    Update Main.java

--- src/main/java/com/alura/comex/Main.java
+++ src/main/java/com/alura/comex/Main.java
@@ -10,12 +10,16 @@ public class Main {
         ProcesadorDeCsv procesador = new ProcesadorDeCsv();
         List<Pedido> pedidos = procesador.leerPedidosDesdeCsv("pedidos.csv");
 
+        // Crear el informe sintético utilizando la lista de pedidos
         InformeSintetico informe = new InformeSintetico(pedidos);
 
+        // Imprimir el informe sintético
         System.out.println("#### INFORME SINTÉTICO");
         System.out.printf("- TOTAL DE PEDIDOS REALIZADOS: %s\n", informe.getTotalDePedidosRealizados());
         System.out.printf("- TOTAL DE PRODUCTOS VENDIDOS: %s\n", informe.getTotalDeProductosVendidos());
         System.out.printf("- TOTAL DE CATEGORÍAS: %s\n", informe.getTotalDeCategorias());
         System.out.printf("- MONTO DE VENTAS: %s\n", informe.getMontoDeVentas());
         System.out.printf("- PEDIDO MÁS BARATO: %s\n", informe.getPedidoMasBarato());
-        System.out.printf("- PEDIDO MÁS CARO: %s\n", informe.getPedido
+        System.out.printf("- PEDIDO MÁS CARO: %s\n", informe.getPedidoMasCaro());
+    }
+}
